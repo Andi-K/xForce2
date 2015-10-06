0f3cf1fb7db24dc42b994692b846e6cca08e1821
Andi <andiana@PC>
2015-10-06 18:01:22 +0200

    + Link to Github

--- src/gameset/demo/src/gameset.gd
+++ src/gameset/demo/src/gameset.gd
@@ -10,12 +10,11 @@ func _ready():
 	
 	var date = OS.get_date()
 	var object_id
-
 	
 	# get_node("/root/Data").objects is a list of all game objetcs
 	var objects = data.objects
 	# the game will load Objects from "gs://objects.dat"
-	# but we can add Objects at runtime:
+	# It's easier to edit the objects.dat but we can add Objects at runtime:
 	object_id = objects.add("STORY", {"image":"gs://res/Mond150.png", "icon":"gs://res/Mond32.png"})
 	# and the translatins for this Object:
 	data.add_trans({
@@ -23,17 +22,20 @@ func _ready():
 			_NAME = "Added at Runtime...",
 			_DESC = "ID: [i]"+ object_id +"[/i]\\nDate: [b]"+ str(date.year) +"-"+ str(date.month) +"-"+ str(date.day) \
 				+"[/b]\\nYour OS: [i][b]"+ OS.get_name() +"[/b][/i]\\n" \
-				+"Config file saved at: [url]file://"+ OS.get_data_dir() +"/game.cfg[/url]",
+				+"Config file saved at: [i][url]file://"+ OS.get_data_dir() +"/game.cfg[/url][/i]" \
+				+"\\n[url=https://github.com/Andi-K/xForce2/blob/master/src/gameset/demo/objects.dat]objects.dat at  GitHub[/url]" \
+				+"\\n[url=https://github.com/Andi-K/xForce2/blob/master/src/gameset/demo/src/gameset.gd]Gameset Script at GitHub[/url]",
 		}
 	}, object_id)
 	
-	# And save it: (Warning:! this owerwites existing files!)
+	# And save it as text file:
+	# WARNING: this owerwites existing files!
 	# gs:// = save into the current gameset 
 	# user:// = save into user's home dir (configs) 
 #	data.save_cfg(objects.as_dict(), "gs://objects_new.dat")
 
 	# the objects.dat don't include translatable Text!
-	# We have to create new translations files (csv)
+	# We have to create new translations files (*.csv)
 	# Don't forget to edit and (re)import them!
 #	for lang in ["en"]:
 #		data.save_tr_str_gs(lang, "gs://src/new_translation")
